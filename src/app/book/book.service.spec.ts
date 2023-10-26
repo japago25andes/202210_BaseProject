7926a078a095f70fb98695c0da6dccc1e9167fe6
John Alexander Pardo Gonzalez <j.pardog@uniandes.edu.co>
2023-10-26 12:24:24 -0500

    finalizacion proyecto

--- src/app/book/book.service.spec.ts
+++ src/app/book/book.service.spec.ts
@@ -2,15 +2,19 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { BookService } from './book.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Book', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [BookService]
-    });
-  });
+ beforeEach(() => {
+   TestBed.configureTestingModule({
+     imports: [HttpClientTestingModule],
+     providers: [BookService]
+   });
+ });
 
-  it('should ...', inject([BookService], (service: BookService) => {
-    expect(service).toBeTruthy();
-  }));
+ it('should ...', inject([BookService], (service: BookService) => {
+   expect(service).toBeTruthy();
+ }));
 });
+
+/* Archivo src/app/book/book.service.spec.ts */

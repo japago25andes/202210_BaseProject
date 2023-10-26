a39ad1c9d7c4f744dc5ba66c5ceccd77ac1e94b9
John Alexander Pardo Gonzalez <j.pardog@uniandes.edu.co>
2023-10-25 22:48:13 -0500

    Avance del proyecto

---
+++ src/app/editorial/editorial.ts
@@ -0,0 +1,10 @@
+export class Editorial {
+    id: number;
+    name: string;
+   
+    constructor(id: number, name: string) {
+      this.id = id;
+      this.name = name;
+    }
+   }
+   /*archivo src/app/editorial/editorial.ts*/
